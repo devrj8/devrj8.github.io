3099fdbfd94e7157278b19c88861444cdfab7c52
dev-rj8 <dev.rjakkani8@gmail.com>
2017-04-26 22:57:39 -0400

    added one more comic
    attack on the titan

--- animedata.service.js
+++ animedata.service.js
@@ -4,7 +4,7 @@ var app = angular.module("AnimeApp");
 
 
 app.service("animeImdbSvc", function(){
-    this.imdbIds = ['tt1355642','tt0877057', 'tt0988824'];
+    this.imdbIds = ['tt1355642','tt0877057', 'tt0988824', 'tt2560140'];
 });
 })();
 
